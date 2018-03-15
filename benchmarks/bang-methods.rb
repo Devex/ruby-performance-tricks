cc5d4b01bda5b2fe4e2615daac8667c2b6f57b1f
Jonas Schulze <jonas.schulze@st.ovgu.de>
2018-03-15 13:02:41 +0100

    Add comparison to bang-method benchmark

--- benchmarks/bang-methods.rb
+++ benchmarks/bang-methods.rb
@@ -13,7 +13,8 @@ array = (0..N).to_a
 
 puts RUBY_DESCRIPTION
 
-Benchmark.bm(10) do |x|
-  x.report("merge!") { merge!(array) }
-  x.report("merge")  { merge(array)  }
+Benchmark.bm(10, "m VS m!") do |x|
+  m_bang = x.report("merge!") { merge!(array) }
+  m      = x.report("merge")  { merge(array)  }
+  [m / m_bang]
 end
